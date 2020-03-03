2914be3fc94768490c55c8c36a9ef3a957dace44
JackMaarek <jacques.maarek@hetic.net>
2020-03-03 16:20:34 +0100

    :tada: Initial Commit

---
+++ elb.tf
@@ -0,0 +1,41 @@
+resource "aws_elb" "test" {
+  name               = "test-terraform-elb"
+  availability_zones = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+
+  access_logs {
+    bucket        = "test"
+    bucket_prefix = "jack"
+    interval      = 60
+  }
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port      = 8000
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "test-terraform-elb"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  description = "API access key"
+  type        = "string"
+}
+
+variable "secret_key" {
+  description = "API secret key"
+  type        = "string"
+}
