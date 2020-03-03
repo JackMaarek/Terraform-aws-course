fd9abc8e00e364f4140de2cc284a96253be3db56
JackMaarek <jacques.maarek@hetic.net>
2020-03-03 17:57:21 +0100

    :art: Fix: Updated readability of files

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,11 +1,16 @@
 resource "aws_s3_bucket" "main-lb-logs" {
   bucket = "test-jack - ${workspace.workspace}"
-  acl    = "private"
-
-  policy = ""
 
   tags = {
-    Name        = "RiskMe LB Logs - ${terraform.workspace}"
-    Environment = "${terraform.workspace}"
+    Name = "LB Logs"
+  }
+}
+
+resource "aws_s3_access_point" "main-lb-logs-exemples" {
+  bucket = "${aws_s3_bucket.example.id}"
+  name   = "example"
+
+  vpc_configuration {
+    vpc_id = "${aws_vpc.example.id}"
   }
 }
